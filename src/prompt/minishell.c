4dd5351c138f8f23ffb324ecb79f508c0094c543
selfix123 <lezach33@hotmail.com>
2023-07-04 08:21:54 -0400

    cbt

--- src/prompt/minishell.c
+++ src/prompt/minishell.c
@@ -10,15 +10,16 @@ int	main(int argc, char **argv, char **env)
 	(void)argv;
 	ft_bzero(&data, sizeof(t_data));
 	cpy_env(&data, env);
-	pwd();
 	i = 0;
-	while (i < 10)
+	while (1)
 	{
 		line = readline("minishell> ");
-		// parsing(line, &data) == FAILURE
-		if (ft_strlen(line) && commands_init(&data, line) == SUCCESS)
-			add_history(line);
+		if (!line)
+			break ;
+		parsing(line, &data);
+		add_history(line);
 		i++;
 	}
+	ft_x2free((void **)data.env);
 	rl_clear_history();
 }
