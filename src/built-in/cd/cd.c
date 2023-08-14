10d55e8a3de820e05e65014624bd1b0566e2b7dc
selfix123 <lezach33@hotmail.com>
2023-08-14 12:07:12 -0400

    cbt merge

--- src/built-in/cd/cd.c
+++ src/built-in/cd/cd.c
@@ -16,34 +16,6 @@ static char	*get_path_name(char **env, char *name)
 	return (NULL);
 }
 
-static void	change_old_pwd(char ***env, char *tmp)
-{
-	char	*new_op;
-	char	**old_pwd;
-
-	old_pwd = ft_calloc(3, sizeof(char *));
-	new_op = tmp;
-	old_pwd[0] = ft_strdup("fuck you criss de fif");
-	old_pwd[1] = ft_strjoin("OLDPWD=", new_op);
-	ft_printf("old_pwd: %s\n", old_pwd[1]);
-	ft_export(old_pwd, env, false, 1);
-	ft_x2free((void **)old_pwd);
-}
-
-static void	change_pwd(char ***env)
-{
-	char	*new_pwd;
-	char	**pwd;
-
-	pwd = ft_calloc(3, sizeof(char *));
-	new_pwd = getcwd(NULL, 0);
-	pwd[0] = ft_strdup("fuck you criss de fif");
-	pwd[1] = ft_strjoin("PWD=", new_pwd);
-	ft_printf("pwd: %s\n", pwd[1]);
-	ft_export(pwd, env, false, 1);
-	ft_x2free((void **)pwd);
-}
-
 static int	go_to_home(char **env)
 {
 	char	*dir;
--- src/built-in/cd/cd.h
+++ src/built-in/cd/cd.h
@@ -5,5 +5,7 @@
 # include "../export/export.h"
 
 int	cd(char **av, char ***env);
+void	change_pwd(char ***env);
+void	change_old_pwd(char ***env, char *tmp);
 
 #endif
